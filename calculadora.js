6737242249c5cdf82a159b000e196f32e1f4fd4e
Matias <matiasruizr@hotmail.es>
2018-12-16 10:21:54 -0400

    destacando seccion calculada

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
 var tiempo, ritmo, distancia
 var btnCalcular = document.getElementById('calcular')
-
+var btnBorrarDatos = document.getElementById('borrarDatos')
 
 const creaFecha = (minutos, segundos) => {
     let total = new Date(1970, 0, 1)
@@ -11,6 +11,22 @@ const creaFecha = (minutos, segundos) => {
         total.setSeconds(segundos)
     }
     return total;
+}
+
+const getRatioDistancia = () => {
+    let km
+    if (document.getElementById('5k_r').checked) {
+        km = document.getElementById('5k_r').value 
+    } else if (document.getElementById('10k_r').checked) {
+        km = document.getElementById('10k_r').value 
+    } else if (document.getElementById('21k_r').checked){
+        km = document.getElementById('21k_r').value 
+    } else if (document.getElementById('42k_r').checked){
+        km = document.getElementById('42k_r').value 
+    } else {
+        km = parseInt(document.getElementById('km_r_d').value) + (parseFloat(document.getElementById('mm_r_d').value / 1000) ) 
+    }
+    return km
 }
 
 const actualizaValores = () => {
@@ -58,28 +74,33 @@ const actualizaValores = () => {
     // Distancia= 
     distancia = {
         metros: document.getElementById('mm_r_d').value,
-        kilometros: document.getElementById('km_r_d').value 
+        kilometros:  getRatioDistancia()
     }
 }
-
-
 
     const calculaDistancia = () => {
         let minutos = (tiempo.calculaTotal().getHours() * 60) + tiempo.calculaTotal().getMinutes() + (tiempo.calculaTotal().getSeconds() / 100)
         let ritmoxMinutos = ritmo.calcularTotalRitmo().getMinutes() +  ( ritmo.calcularTotalRitmo().getSeconds() / 60)
-        // alert( 'La distancia es de ' + minutos / ritmoxMinutos + 'Kilometros' )
-        // document.getElementById('km_r_d').innerHTML(minutos / ritmoxMinutos)  
         document.getElementById('km_r_d').value = Math.floor(minutos / ritmoxMinutos)
         document.getElementById('mm_r_d').value = minutos / ritmoxMinutos - Math.floor(minutos / ritmoxMinutos)
+        document.getElementById('divDistancia').className = 'categoria bgReverse'
+        alert("Estoy calculando la distancia "+!distancia.kilometros )
+
     }
     const calculaRitmo = () => {
+        alert(distancia.kilometros)
         let minutos = (tiempo.calculaTotal().getHours() * 60) + tiempo.calculaTotal().getMinutes() + (tiempo.calculaTotal().getSeconds() / 60)
         alert( 'el ritmo es de ' + minutos / distancia.kilometros + ' minutos por kilometro' )
+        let imprMinutos  = (60 * (minutos / distancia.kilometros - Math.floor(minutos / distancia.kilometros)) ).toString()
         // document.getElementById('minutos_vel').value  = Math.floor(minutos / distancia.kilometro) 
+        document.getElementById('minutos_vel').value =  Math.floor(minutos / distancia.kilometros)
+        document.getElementById('segundos_vel').value =  Math.floor(imprMinutos)
+        document.getElementById('divRitmo').className = 'categoria bgReverse'
     }
     const calculaTiempo = () => {
         alert( 'el tiempo es de ' + ritmo.calcularTotalRitmo().getMinutes() * distancia.kilometros   + ' minutos ' )
-        document.getElementById('minutos_r_t').value = ritmo.calcularTotalRitmo().getMinutes() * distancia.kilometros
+        document.getElementById('minutos_rt').value = Math.floor(ritmo.calcularTotalRitmo().getMinutes() * distancia.kilometros) 
+        document.getElementById('divTiempo').className = 'categoria bgReverse'
     }
 
 
@@ -89,6 +110,8 @@ btnCalcular.onclick = function() {
     console.log(`Tiempo:    ${tiempo.horas} horas ${tiempo.minutos} minutos    ${tiempo.segundos} segundos`)
     console.log(`Distancia: ${distancia.kilometros}   kilometros ${distancia.metros}   metros`)
 
+
+  
     if(!distancia.kilometros){
         calculaDistancia()
     }else if (!tiempo.horas){
@@ -96,6 +119,24 @@ btnCalcular.onclick = function() {
     }else if (!ritmo.minutos){
         calculaRitmo()
     } else{
-        alert("Debes ingresar dos de 3 valores")
-    }
+    // Mensaje error    
+    }    
+     
+}
+
+btnBorrarDatos.onclick = function() {
+    document.getElementById('km_r_d').value = ''
+    document.getElementById('mm_r_d').value= ''
+
+    document.getElementById('minutos_r_t').value = ''
+    document.getElementById('horas_r_t').value = ''
+    document.getElementById('segundos_r_t').value = ''
+
+    document.getElementById('minutos_vel').value = ''
+    document.getElementById('segundos_vel').value= ''
+
+    document.getElementById('5k_r').checked = false;
+    document.getElementById('10k_r').checked = false;
+    document.getElementById('21k_r').checked = false;
+    document.getElementById('42k_r').checked = false;
 }
